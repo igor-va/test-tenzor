0e0f399262ff074fabe4eec40e23ce7f46ca6cf2
Igor <igoryan16031991@gmail.com>
2025-02-13 16:10:17 +0300

    Start project

---
+++ configurations/TestData.py
@@ -0,0 +1,10 @@
+import os
+
+
+class TestData:
+    URL = "https://barleone.ru/"
+    URL_LOGIN = URL + "authorization/"
+    URL_CATALOG = URL + "shop/"
+    URL_CONTACTS = URL + "contacts/"
+    USERNAME = ""
+    PASSWORD = ""
---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(params=['Chrome'], scope="class")
+def fixture_setup(request):
+    """Return driver browser"""
+    driver = None
+    if request.param == "Chrome":
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--start-maximized")
+        # chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+    elif request.param == "Firefox":
+        firefox_options = webdriver.FirefoxOptions()
+        # firefox_options.add_argument("--headless")
+        driver = webdriver.Firefox(options=firefox_options)
+        driver.maximize_window()
+    yield driver
+    driver.quit()
